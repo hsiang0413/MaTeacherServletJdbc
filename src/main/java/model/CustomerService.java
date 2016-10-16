d21c983c054ba4a4fd77cb1b7f6cd1fd1f2b2ae5
Hsiang <lovejune413@hotmail.com>
2016-10-16 23:53:45 +0800

    first commit

---
+++ src/main/java/model/CustomerService.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.Arrays;
+
+import model.dao.CustomerDAOJdbc;
+
+public class CustomerService {
+	private CustomerDAO customerDao = new CustomerDAOJdbc();
+	public static void main(String[] args) {
+		CustomerService customerService = new CustomerService();
+		CustomerBean bean = customerService.login("Babe", "BBB");
+		System.out.println(bean);
+		
+		customerService.changPassword("Ellen", "E", "ABC");
+		
+	}
+	public boolean changPassword(
+			String username, String oldPassword, String newPassword) {
+		CustomerBean bean = this.login(username, oldPassword);
+		if(bean!=null) {
+			byte[] temp = newPassword.getBytes();	//使用者輸入
+			return customerDao.update(
+					temp, bean.getEmail(), bean.getBirth(), username);
+		}
+		return false;
+	}
+	public CustomerBean login(String username, String password) {
+		CustomerBean bean = customerDao.select(username);
+		if(bean!=null) {
+			if(password!=null && password.length()!=0) {
+				byte[] pass = bean.getPassword();	//資料庫抓出
+				byte[] temp = password.getBytes();	//使用者輸入
+				if(Arrays.equals(pass, temp)) {
+					return bean;
+				}
+			}
+		}
+		return null;
+	}
+}
